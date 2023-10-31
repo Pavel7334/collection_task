0d72513c69607ebf46d746833487f0a9216a3f9f
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-10-31 16:42:16 +0400

    Решена 3 задача 1 часть

--- app/chapter_1/router.py
+++ app/chapter_1/router.py
@@ -1,40 +1,39 @@
 from fastapi import HTTPException, APIRouter
 
 router = APIRouter(
-  tags=["Задачт 1-10"],
+    tags=["Задачт 1-10"],
 )
 
-
 alphabet = {
-  "A": "Alfa",
-  "B": "Bravo",
-  "C": "Charlie",
-  "D": "Delta",
-  "E": "Echo",
-  "F": "Foxtrot",
-  "G": "Golf",
-  "H": "Hotel",
-  "I": "India",
-  "J": "Juliett",
-  "K": "Kilo",
-  "L": "Lima",
-  "M": "Mike",
-  "N": "November",
-  "O": "Oscar",
-  "P": "Papa",
-  "Q": "Quebec",
-  "R": "Romeo",
-  "S": "Sierra",
-  "T": "Tango",
-  "U": "Uniform",
-  "V": "Victor",
-  "W": "Whiskey",
-  "X": "X-ray",
-  "Y": "Yankee",
-  "Z": "Zulu",
+    "A": "Alfa",
+    "B": "Bravo",
+    "C": "Charlie",
+    "D": "Delta",
+    "E": "Echo",
+    "F": "Foxtrot",
+    "G": "Golf",
+    "H": "Hotel",
+    "I": "India",
+    "J": "Juliett",
+    "K": "Kilo",
+    "L": "Lima",
+    "M": "Mike",
+    "N": "November",
+    "O": "Oscar",
+    "P": "Papa",
+    "Q": "Quebec",
+    "R": "Romeo",
+    "S": "Sierra",
+    "T": "Tango",
+    "U": "Uniform",
+    "V": "Victor",
+    "W": "Whiskey",
+    "X": "X-ray",
+    "Y": "Yankee",
+    "Z": "Zulu",
 }
 
-
+# 1
 """Напишите вьюшку для запросов типа /letter/<letter>, которая возвращает букву"""
 
 
@@ -47,15 +46,26 @@ def letter_view(letter: str):
     return {"result": result}
 
 
+# 2
 """Напишите вьюшку для запросов типа /find/?letter=<letter>, которая возвращает букву на основе квери-параметра."""
 
 
 @router.get("/find/")
 async def find_view(letter: str):
-
     result = alphabet.get(letter.upper())
 
     if not result:
-        raise HTTPException(status_code=404, detail="Not Found")
+        raise HTTPException(status_code=404, detail="Сервер не найден")
 
     return {"result": result}
+
+# 3
+"""Напишите вьюшку для запросов  /check/<letter>/<word> для проверки соответствия буквы ее расшифровке:"""
+
+
+@router.get("/check/{letter}/{word}")
+async def check_view(letter: str, word: str):
+
+    result = alphabet.get(letter.upper())
+    return result.lower() == word.lower()
+
