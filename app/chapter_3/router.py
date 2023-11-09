b0cdc1ab63ba48a43b794e8b97a3d8dc70a67881
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-11-09 23:52:38 +0400

    Решена 2, 3, 4, 5 задача часть 3

--- app/chapter_3/router.py
+++ app/chapter_3/router.py
@@ -19,4 +19,67 @@ async def increase_value():
     return data
 
 
+# 2
+"""У вас есть список, который хранится в переменной items. Напишите вьюшку, которая добавит элемент 
+из json-тела POST запроса и вернет список"""
 
+items = ["alpha", "bravo", "charlie"]
+
+
+@router.post("/")
+async def add_items(new_items: str):
+    items.append(new_items)
+    return items
+
+
+# 3
+"""У вас есть переменная, где хранится список словарей. Напишите вьюшку, которая добавит элемент 
+из json-тела POST запроса и вернет список. """
+
+enrollments = [
+    {"name": "alex", "phone": "+123456789"},
+]
+
+
+@router.get("/enrollments")
+async def get_enrollments():
+    return enrollments
+
+
+@router.post("/enrollments")
+async def add_enrollments(new_enrollments: dict) -> list:
+    enrollments.append(new_enrollments)
+    return enrollments
+
+
+# 4
+"""У вас есть переменная, где хранится список пользователей. У каждого пользователя есть уникальный идентификатор. 
+При отправке POST запроса добавьте пользователя в список с очередным pk. Верните словарь с пользователем. """
+
+users = [
+    {"pk": 1, "name": "alex", "phone": "+123456789"},
+    {"pk": 2, "name": "mary", "phone": "+987654321"}
+]
+
+
+@router.post("/users")
+async def add_user(new_user: dict) -> list:
+    users.append(new_user)
+    return users
+
+
+# 5
+"""У вас есть переменная, где хранится список пользователей. При отправке POST запроса проверьте что: - [ ]  имя (name) указано
+- [ ]  телефон (phone) указан. Если все ок – добавьте пользователя в список и верните в ответ добавленный словарь.
+Если нет – верните список ошибок в формате “name missed”, “phone missed”"""
+
+new_users = [
+    {"name": "alex", "phone": "+123456789"},
+    {"name": "mary", "phone": "+987654321"}
+]
+
+
+@router.post("/users")
+async def add_user(new_user: dict) -> list:
+    users.append(new_user)
+    return users
