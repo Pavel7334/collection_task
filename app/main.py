673b9110b15f24b6b9bd26de76b1015061221a6d
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-11-15 22:57:08 +0400

    Решена 1-4 задача часть 4

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.chapter_1.router import router as router_chapter_1
 from app.chapter_2.router import router as router_chapter_2
 from app.chapter_3.router import router as router_chapter_3
+from app.chapter_4.router import router as router_chapter_4
 
 app = FastAPI(
     title="Сборник задач"
@@ -12,3 +13,5 @@ app = FastAPI(
 app.include_router(router_chapter_1)
 app.include_router(router_chapter_2)
 app.include_router(router_chapter_3)
+app.include_router(router_chapter_4)
+
