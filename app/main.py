4f9c99d242f860cbb31002d8cf2ac0bedad5ec49
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-11-22 16:28:00 +0400

    Решена 1 задача, 1-й главы, 2-й части

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ from app.part_1.chapter_2.router import router as router_chapter_2
 from app.part_1.chapter_3.router import router as router_chapter_3
 from app.part_1.chapter_4.router import router as router_chapter_4
 
+from app.part_2.chapter_1.task_1.router import router as router_chapter_5
+
 app = FastAPI(
     title="Сборник задач"
 )
@@ -14,4 +16,5 @@ app.include_router(router_chapter_1)
 app.include_router(router_chapter_2)
 app.include_router(router_chapter_3)
 app.include_router(router_chapter_4)
+app.include_router(router_chapter_5)
 
