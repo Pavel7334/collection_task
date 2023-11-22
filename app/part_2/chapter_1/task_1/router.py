31381f35a406a4d571fac7cc30350450bf04a86f
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-11-22 17:28:57 +0400

    Решена 3 задача, 1-й главы, 2-й части

--- app/part_2/chapter_1/task_1/router.py
+++ app/part_2/chapter_1/task_1/router.py
@@ -7,11 +7,19 @@ router = APIRouter(
           " Задачи 1-6"],
 )
 
+# 1
+"""Создайте приложение, которое при обращении на /api/1/random вернет случайное число от 1 до 10 в виде {”number”: N}"""
+
 
 @router.get('/api/1/random/')
 def view_random():
     value = random.randint(1, 10)
     return {"number": value}
+
+
+# 2
+"""Создайте приложение, которое при обращении на /api/1/random?from=…&to=… вернет случайное число от X до Y в виде 
+{”number”: N}. Если параметры from и to не заданы, их значения по умолчанию 1 и 100 соответственно."""
 
 
 @router.get('/api/1/random')
@@ -22,3 +30,14 @@ def random_num(num1=None, num2=None):
     else:
         value = random.randint(int(num1), int(num2))
         return {"number": value}
+
+
+# 3
+"""У вас есть список покупок. Выведите его при обращении на GET /api/1/grocery"""
+
+grocery = ["milk", "sugar", "cookies", "corn-flakes", "nutella"]
+
+
+@router.get('/api/1/grocery')
+def random_grocery():
+    return grocery
