e1ff5c250c034c793caed6bc91e9d00d2b07feee
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-11-29 23:25:44 +0400

    Решена 7 задача, 2-й главы, 2-й части

--- app/part_2/chapter_2/router.py
+++ app/part_2/chapter_2/router.py
@@ -26,7 +26,24 @@ data = [
 @router.get('/api/1/products/{product_name}')
 def info_product(product_name):
     for row in data:
-        if row[1] == product_name:
+        if row[1] == product_name.capitalize():
             return row
     else:
         return "Такого продукта нет"
+
+
+# 7
+"""Верните все продукты в категории в виде списка. Если категория не указана – верните вообще все. 
+GET /api/1/products/?cat=<cat>"""
+
+
+@router.get('/api/1/products/')
+def get_product(product_cat):
+    list_cat = []
+    for row in data:
+        if row[2] == product_cat:
+            list_cat.append(row)
+    return list_cat or data
+
+
+
