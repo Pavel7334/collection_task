a465a3b7f8d8c76f1f8890f2004364360956c7d0
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-12-06 17:57:16 +0400

    Решена 2 задача, 2_2-й главы, 2-й части

--- app/part_2/chapter_2_2/router.py
+++ app/part_2/chapter_2_2/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Header
 
 router = APIRouter(
     prefix="/pages",
@@ -14,6 +14,26 @@ router = APIRouter(
 Выведите значение заголовка в теле ответа"""
 
 
+# @router.get("/")
+# def get_my_header(my_header: str):
+#     return my_header
+
+
+# 2
+"""Напишите приложение, которое обрабатывает запросы
+
+В HTTP запросе передается заголовок `From` – этот редко использующийся заголовок предоставляет серверу адрес владельца 
+
+клиента – его электронную почту. Разделите значение заголовка на имя пользователя и почту и выведите в формате JSON.
+
+Если заголовок не отправлен или не содержит `@`, верните ошибку `400`."""
+
+
 @router.get("/")
-async def get_my_header(my_header: str):
-    return my_header
+async def read_from_header(from_header: str = Header(None)):
+    if not from_header or '@' not in from_header:
+        raise HTTPException(status_code=400, detail="Ошибка сервера")
+
+    mail_name, mail_domain = from_header.split('@')
+    return {"mail_name": mail_name, "mail_domain": mail_domain}
+
